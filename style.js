e35fa6155d2fd7a33ed1ce5c8b34bb6ae4934be3
aryelciu001 <ryelcius.alfredo29@gmail.com>
2018-10-05 12:14:45 +0800

    good

--- style.js
+++ style.js
@@ -4,5 +4,28 @@ window.addEventListener("scroll", function() {
   } else {
     document.getElementsByClassName("navbar")[0].style.backgroundColor = "";
   }
-console.log(pageYOffset)
 });
+
+
+var sectionIntro = document.getElementsByClassName("section")[0];
+var intro = document.getElementById("box2");
+
+var rect;
+var y;
+
+window.addEventListener("scroll",function(){
+	rect = sectionIntro.getBoundingClientRect();
+	intro.style.transition = "2s"
+	y = rect.top;
+	
+	if(y >= 250){
+		intro.style.opacity = "0";
+		intro.style.transform = "translate3D(0,300px,0)"
+	}
+	else{
+		intro.style.opacity = "1";
+		intro.style.transform = "translate3D(0,0,0)"
+	}
+})
+
+
