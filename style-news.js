85160975e3c15c3972806ef7eceddab420270311
Jungha Cho <43437031+Harvendois@users.noreply.github.com>
2018-10-06 17:56:36 +0900

    Update 1.1 for NEWS
    Still bunches of errors and fixes needed

---
+++ style-news.js
@@ -0,0 +1,31 @@
+window.addEventListener("scroll", function() {
+    if (this.pageYOffset > 100) {
+      document.getElementsByClassName("navbar")[0].style.backgroundColor = "white";
+    } else {
+      document.getElementsByClassName("navbar")[0].style.backgroundColor = "";
+    }
+  });
+  
+  var heart = document.getElementsByClassName("fa-heart")[0];
+  var sectionIntro = document.getElementsByClassName("section")[0];
+  var intro = document.getElementById("box2");
+  
+  var rect;
+  var y;
+  
+  window.addEventListener("scroll",function(){
+      rect = sectionIntro.getBoundingClientRect();
+      intro.style.transition = "2s"
+      y = rect.top;
+      
+      if(y >= 250){
+          intro.style.opacity = "0";
+          intro.style.transform = "translate3D(0,300px,0)"
+      }
+      else{
+          intro.style.opacity = "1";
+          intro.style.transform = "translate3D(0,0,0)"
+      }
+  })
+  
+  
