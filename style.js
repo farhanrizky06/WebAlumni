0c94e89eab2845dfcf9aa36c14f43dc3f3390964
aryelciu001 <ryelcius.alfredo29@gmail.com>
2018-10-13 00:25:11 +0800

    typewriter effect + responsive already

--- style.js
+++ style.js
@@ -1,6 +1,7 @@
 window.addEventListener("scroll", function() {
   if (this.pageYOffset > 100) {
-    document.getElementsByClassName("navbar")[0].style.backgroundColor = "black";
+    document.getElementsByClassName("navbar")[0].style.backgroundColor =
+      "black";
   } else {
     document.getElementsByClassName("navbar")[0].style.backgroundColor = "";
   }
@@ -13,31 +14,47 @@ var intro = document.getElementById("box2");
 var rect;
 var y;
 
-window.addEventListener("scroll",function(){
-	rect = sectionIntro.getBoundingClientRect();
-	intro.style.transition = "2s"
-	y = rect.top;
-	
-	if(y >= 250){
-		intro.style.opacity = "0";
-		intro.style.transform = "translate3D(0,300px,0)"
-	}
-	else{
-		intro.style.opacity = "1";
-		intro.style.transform = "translate3D(0,0,0)"
-	}
-})
+window.addEventListener("scroll", function() {
+  rect = sectionIntro.getBoundingClientRect();
+  intro.style.transition = "2s";
+  y = rect.top;
 
+  if (y >= 250) {
+    intro.style.opacity = "0";
+    intro.style.transform = "translate3D(0,300px,0)";
+  } else {
+    intro.style.opacity = "1";
+    intro.style.transform = "translate3D(0,0,0)";
+  }
+});
 
-function landingPageTextAnimation(x){	
-	x.style.opacity = 1;
+function landingPageTextAnimation(x) {
+  x.style.opacity = 1;
 }
 
 var landingPageText = document.getElementsByClassName("theTextBox")[0];
-if(landingPageText == undefined){console.log('nil')}
-else{
-	setTimeout(function(){
-	landingPageTextAnimation(landingPageText)},500)
+if (landingPageText == undefined) {
+  console.log("nil");
+} else {
+  setTimeout(function() {
+    landingPageTextAnimation(landingPageText);
+  }, 500);
 }
 
+var typewriter = document.getElementById("typewriter");
 
+if (typewriter == undefined) {
+  console.log("error typewriter");
+} else {
+  setInterval(() => {
+    if (typewriter.innerHTML == "Alumni") {
+      typewriter.innerHTML = "Friends";
+    } else if (typewriter.innerHTML == "Friends") {
+      typewriter.innerHTML = "Menthors";
+    } else if (typewriter.innerHTML == "Menthors") {
+      typewriter.innerHTML = "Family";
+    } else if (typewriter.innerHTML == "Family") {
+      typewriter.innerHTML = "Alumni";
+    }
+  }, 2500);
+}
