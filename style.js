05ad42ddd53e039ec9aece9efa9d66854fecdfad
Jungha Cho <43437031+Harvendois@users.noreply.github.com>
2018-10-15 23:14:24 +0900

    color and transition change needed

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function() {
   if (this.pageYOffset > 100) {
     document.getElementsByClassName("navbar")[0].style.backgroundColor =
-      "black";
+      "#203257";
   } else {
     document.getElementsByClassName("navbar")[0].style.backgroundColor = "";
   }
